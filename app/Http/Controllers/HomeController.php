c925ebe7394735cf25c78ea3cdf657c63cb9d409
Marc Garcia <garciatorrent96@gmail.com>
2017-03-21 16:39:33 +0100

    Testing messages

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -17,6 +17,33 @@ class HomeController extends Controller
 	        if ($hub_verify_token === env('MESSENGER_TOKEN'))
 	            echo $challenge;
         }
+
+        $input = json_decode(file_get_contents('php://input'), true);
+
+		// Get the Senders Graph ID
+		$sender = $input['entry'][0]['messaging'][0]['sender']['id'];
+
+		// Get the returned message
+		$message = $input['entry'][0]['messaging'][0]['message']['text'];
+
+		//API Url and Access Token, generate this token value on your Facebook App Page
+		$url = 'https://graph.facebook.com/v2.6/me/messages?access_token='.env('PAGE_TOKEN');
+
+		//Initiate cURL.
+		$ch = curl_init($url);
+
+		//The JSON data.
+		$jsonData = '{
+		    "recipient":{
+		        "id":"' . $sender . '"
+		    }, 
+		    "message":{
+		        "text":"The message you want to return"
+		    }
+		}';
+
+		//Tell cURL that we want to send a POST request.
+		curl_setopt($ch, CURLOPT_POST, 1);
          
     }
 }
