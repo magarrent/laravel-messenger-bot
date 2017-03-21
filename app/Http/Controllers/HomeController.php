ddec348f0a48d1e3da11d186dd0c7aa80d387868
Marc Garcia <garciatorrent96@gmail.com>
2017-03-21 16:11:20 +0100

    Verify token

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+
+class HomeController extends Controller
+{
+    public function getWebhook() {
+        if(isset($_REQUEST['hub_challenge'])) {
+            $challenge = $_REQUEST['hub_challenge'];
+            $hub_verify_token = $_REQUEST['hub_verify_token'];
+        }
+         
+         
+        if ($hub_verify_token === env('FACEBOOK_TOKEN'))
+            echo $challenge;
+    }
+}
